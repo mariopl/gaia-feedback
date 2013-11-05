61d96abc4bd0e1a0131839141a80b920b6a88b89
Mario Pierre-Louis Marín <b.mplm@tid.es>
2013-11-05 11:01:45 +0100

    solving problem with language

--- js/feedback.js
+++ js/feedback.js
@@ -20,62 +20,34 @@ var Feedback = {
 
     var contact = document.getElementById('contact').value;
 
-    var mozMobileConnection = navigator.mozMobileConnection;
-    if (!mozMobileConnection) {
-      window.alert(navigator.mozL10n.get('error'));
-      return;
+    // @type: error || success
+    var message = function(type) {
+        var status = document.getElementById(type+'Msg');
+        status.classList.remove('hidden');
+        var delay = setTimeout(function() {
+         status.classList.add('hidden');
+        }, 2000);
     }
 
-    var req = mozMobileConnection.sendMMI('*#06#');
-    req.onsuccess = (function onIMEI() {
-      var formData = new FormData();
-      formData.append('build_id', navigator.buildID);
-
-      var comment = document.getElementById('feedback-textarea');
-      formData.append('comment', comment.value);
-
-      var imei = req.result;
-      if (imei) {
-        formData.append('imei', imei);
-      }
-
-      if (contact) {
-        formData.append('contact', contact);
-      }
-
-      // @type: error || success
-      var message = function(type) {
-          var status = document.getElementById(type+'Msg');
-          status.classList.remove('hidden');
-          var delay = setTimeout(function() {
-           status.classList.add('hidden');
-          }, 2000);
-      }
-
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', DOGFOOD_URL);
-      xhr.addEventListener('load', function() {
-        if (xhr.status === 200 || xhr.status === 0) {
-          message("success");
-        } else {
-          message("error");
-        }
-      });
-      xhr.onerror = function () {
-        if (xhr.status === 0) {
-          message("success");
-          return;
-        }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', DOGFOOD_URL);
+    xhr.addEventListener('load', function() {
+      if (xhr.status === 200 || xhr.status === 0) {
+        message("success");
+      } else {
         message("error");
-      };
-      xhr.send(formData);
-      this.reset();
-    }).bind(this);
-    req.onerror = function onerror() {
-      window.alert(navigator.mozL10n.get('error'));
-      return;
-    }
-  }
+      }
+    });
+    xhr.onerror = function () {
+      if (xhr.status === 0) {
+        message("success");
+        return;
+      }
+      message("error");
+    };
+    xhr.send(formData);
+    this.reset();
+  });
 };
 
 window.addEventListener('load', function fbLoad(evt) {
